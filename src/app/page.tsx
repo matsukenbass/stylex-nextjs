bf0c16a7c70d9bfb1d6a81100552a899afe64532
matsukenbass <matsukenbass@gmail.com>
2024-02-11 16:26:21 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,67 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import stylex from '@stylexjs/stylex';
+
+const fadeIn = stylex.keyframes({
+  from: { opacity: 0 },
+  to: { opacity: 1 },
+});
+const s = stylex.create({
+  base: {
+    fontSize: 16,
+    lineHeight: 1.5,
+    color: 'rgb(60,60,60)',
+    animationName: fadeIn,
+    animationDuration: '1s',
+  },
+  bar: (height) => ({
+    height,
+  }),
+  highlighted: {
+    color: 'rebeccapurple',
+  },
+  redbox: {
+    backgroundColor: 'red',
+    width: 100,
+    height: 100,
+  },
+  button: {
+    color: {
+      default: 'var(--blue-link)',
+      ':hover': {
+        default: null,
+        '@media (hover: hover)': 'scale(1.1)',
+      },
+      ':active': 'scale(0.9)',
+    },
+    backgroundColor: {
+      default: 'lightblue',
+      ':hover': 'blue',
+      ':active': 'darkblue',
+    },
+  },
+  input: {
+    // pseudo-element
+    '::placeholder': {
+      color: '#999',
+    },
+    color: {
+      default: '#333',
+      // pseudo-class
+      ':invalid': 'red',
+    },
+  },
+  header: {
+    position: stylex.firstThatWorks('sticky', '-webkit-sticky', 'fixed'),
+  },
+});
 
 export default function Home() {
+  const height = 10;
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <div {...stylex.props(s.bar(height))} />
+      <div className={stylex(s.redbox)} />
+      <input className={stylex(s.input)}></input>
+      <button className={stylex(s.button)}>push</button>
     </main>
-  )
+  );
 }
